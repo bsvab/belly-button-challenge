bad5843475f65b0e1a0f018cf6ba33df38e168f9
bsvab <bnsvab@gmail.com>
2024-02-14 21:27:37 -0600

    progress

--- static/js/app.js
+++ static/js/app.js
@@ -12,25 +12,28 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+// const dataPromise = d3.json(url);
+// console.log("Data Promise: ", dataPromise);
 //\\//\\ NEED A BETTER UNDERSTANDING OF WHAT ^ABOVE LINE^ DOES AND WHY TURNING AROUND AND DOING .JSON IN THE VERY NEXT LINE WITH THE .THEN DOESN'T NEGATE WHATEVER THIS IS DOING
 
 // Fetch the JSON data, console log it, define variables from it
 d3.json(url).then(function(data) {
-    console.log(data);
-});
+  console.log(data);
 
-// values for the bar chart
-let sample_values = data.map(function() {
+  // values for the bar chart
+  let sample_values = data.map(function() {
     //...
+  });
+
+  // labels for the bar chart
+  let otu_ids = Object.values(data.names);
+
+  // hovertext for the chart
+  let otu_labels = //...
+  
 });
 
-// labels for the bar chart
-let otu_ids = Object.values(data.names);
 
-// hovertext for the chart
-let otu_labels = //...
 
 // ------------------------------------------------------------------------------------------------------------------------------------
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
